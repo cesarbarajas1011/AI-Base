27e2a91f3002a7b83b2832d8ee0e8204b2d72390
Cesar Barajas <cesarbarajas@Mac.lan>
2026-02-12 10:31:46 -0500

    changes

--- langChain.py
+++ langChain.py
@@ -2,8 +2,13 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 llm = ChatGroq(model="llama-3.3-70b-versatile", temperature=0,
-                 groq_api_key="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")
+                 groq_api_key=os.getenv("GROQ_API_KEY"))
 
 template = """What is the capital of France?"""
 
